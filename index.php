616c420791221fae2a079986ecb872b691065884
Jim D <jimdough@Jim-Dough-CPT-MacBook-Pro.local>
2016-05-03 16:20:58 -0400

    First Page

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+
+  <title>AIT - Truck Driving School</title>
+  <?php include("inc/head.inc"); ?>
+    <link rel="stylesheet" type="text/css" href="/slick/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="/slick/slick-theme.css"/>
+	
+
+ 
+  <META NAME="DESCRIPTION" CONTENT="Insert the description here.">
+    
+</head>
+<body>
+	<!-- Start: Wrapper --> 
+	<div class="wrapper">
+		<!-- Start: Header -->
+		 <header id="header">
+		 		<div class="row" >
+			 		  <?php include("inc/nav.inc"); ?>
+		 		</div>
+		 </header>
+		 <!-- End: Header -->
+		 
+		 <div class="row">
+			 <div class="columns small-12">
+				 <div class="hpSlider">
+					  <div><img src="/img/Draft-Mission.jpg"></div>
+					  <div><img src="/img/Draft-Contact.jpg"></div>
+					  <div><img src="/img/Draft-Focus.jpg"></div>
+				 </div>
+			 </div>
+		 </div>
+		 
+		 
+
+		 	<div class="row">
+		 			<div class="small-12 large-5 columns">
+		 			<h4>Nationally Recognized Training</h4>
+					<p>AIT is accredited by the Accrediting Commission of Career Schools and Colleges, is PTDI Certified and has had an A+ rating with the Better Business Bureau for over 20 years. Learn More</p>
+					<h4>Start Your Future in Trucking</h4>
+					<p>Professional driving skills can enable graduates the opportunity to advance. Successful completion of the program initially qualifies graduates for a wide range of trucking jobs.</p>
+					<p><a href="cdl-course-information.php" class="button small">Learn More</a></p>
+		 			</div>
+		 			<div class="small-12 large-7 columns">
+			 			<img src="/migration/feat.jpg" alt="AIT Graduate" />
+		 			</div>
+		 	</div>
+		
+		<br><br>
+		
+		<div class="row">	
+
+			
+			<div class="column large-5">
+					<h4>Training Professional Truck Drivers for 25 years!</h4>
+		<p>American Institute of Trucking (AIT) is an organization dedicated to training individuals to be safe and professional truck drivers. The trucking industry offers many competitive benefits such as health insurance, paid vacations, and retirement plans making it one of the highest sought after careers available. AIT is accredited by the Accrediting Commission of Career Schools and Colleges, with an A+ rating with the Better Business Bureau.</p>
+			</div>
+			
+						<div class="column large-7">
+				<div class="flex-video widescreen"><iframe width="853" height="480" src="https://www.youtube.com/embed/F9E6wZKDUeQ?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<h5 class="text-center">New Truck Drivers Can Earn Up To $40,900<span class="asterisk">*</span> In Their First Year!</h5>
+				<p class="disclaimer text-center">*as supplied by the carriers that hire our the majority of our graduates - Werner and Covenant</p>
+			</div>
+
+		</div> 		 
+		
+		<div class="row">
+			<div class="column small-12">
+				<?php include("contact-horizontal.php"); ?>
+			</div>
+		</div>
+		
+
+ 		 
+ 		 		  <?php include("inc/footer.inc"); ?>
+ 		 		  
+ 	</div>
+	<!-- End: Wrapper -->
+
+  <?php include("inc/footer-scripts.inc"); ?>
+  <script type="text/javascript" src="/slick/slick.min.js"></script>
+  	  <script type="text/javascript">
+    $(document).ready(function(){
+	$('.hpSlider').slick({
+		  infinite: true,
+		  slidesToShow: 1,
+		  slidesToScroll: 1,
+		  autoplay: true,
+		  autoplaySpeed: 4000,
+		  speed: 900,
+		  fade: true,
+		  cssEase: 'linear'
+		});
+				    });
+  </script>
+
+</body>
+</html>
